314901cb1c88611906e1effd9a62f512d9a5c1e3
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2023-04-26 14:43:58 +0100

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'pelumiisola87@gmail.com',
-        pass: 'mhlanjtetwxvqvfz'
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
     }
 });
 
 const welcomeMail = (email, name) => {
     let mailOptions = {
-        from: 'pelumiisola87@gmail.com',
+        from: process.env.EMAIL,
         to: email,
         subject: 'Thanks for Joining in!',
         text: `Welcomr to the App, ${name}. Let me know how you get along with the app.`
@@ -33,6 +33,20 @@ const cancelMail = (email, name) => {
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. I hope to see you sometime soon.`
     };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+module.exports = { 
+    welcomeMail,
+    cancelMail
+}
     
     transporter.sendMail(mailOptions, function(error, info){
         if (error) {
